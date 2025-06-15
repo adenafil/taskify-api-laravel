419f15aff4d0bce5403521b25547d6b94762dd94
adenafil <nafilie9@gmail.com>
2025-06-15 22:23:02 +0700

    check ip

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,33 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (Request $request) {
+function get_client_real_ip() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        // IP dari shared ISP
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // IP dari proxy
+        $ip_list = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        foreach ($ip_list as $ip) {
+            $ip = trim($ip);
+            if (filter_var($ip, FILTER_VALIDATE_IP)) {
+                return $ip;
+            }
+        }
+    } elseif (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        // IP dari Cloudflare
+        return $_SERVER['HTTP_CF_CONNECTING_IP'];
+    } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+        // Fallback ke REMOTE_ADDR
+        return $_SERVER['REMOTE_ADDR'];
+    }
 
-    return "API is active on /api endpoint" .  $request->getClientIp() ;
+    return null;
+}
+
+Route::get('/', function (Request $request) {
+    $clientIp = get_client_real_ip();
+    return "API is active on /api endpoint" .  $clientIp;
 });
 
 Route::get('/login/{service}', [AuthSocialiteController::class, 'redirect']);
